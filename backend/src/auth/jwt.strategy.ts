ef31456ae6cd8b30b698ebe5a2bf35606f3dccdc
yati-kasera <kasera29th@gmail.com>
2025-06-30 19:29:57 +0530

    backend lint errors resloved

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,36 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import { PassportStrategy } from '@nestjs/passport';
-// import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
-
-// interface JwtPayload {
-//   sub: string;
-//   email: string;
-//   username: string;
-// }
-
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   constructor() {
-//     // super({
-//     const opts: StrategyOptions = {
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: 'SECRET_KEY', // TODO: use env variable
-//     // });
-//     };
-//     super(opts);
-//   }
-
-//   validate(payload: JwtPayload) {
-//     return { userId: payload.sub, email: payload.email, username: payload.username };
-//   }
-// } 
-
-
-
-
-
-
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
